23f608812870bba83f88015690f8075cb30ee63d
Nestor <zozomezo@abv.bg>
2023-12-06 22:47:15 +0200

    Sample nav items and hamburger

--- src/app/ui/navbar.js
+++ src/app/ui/navbar.js
@@ -1,14 +1,41 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import styles from './navbar.module.css'
 
 // import Logo from './Logo'
 // import Button from './Button'
 
 export function Navbar() {
     return (
-        <div className="">
-            <div className="">
-                <div className="">
+        <nav className={styles.navbar}>
+            <div className={`${styles.navbarContainer} ${styles.container}`}>
+                <input type="checkbox" name="" id="" />
+                <div className={styles.hamburgerLines}>
+                    <span className={`${styles.line} ${styles.line1}`}></span>
+                    <span className={`${styles.line} ${styles.line2}`}></span>
+                    <span className={`${styles.line} ${styles.line3}`}></span>
+                </div>
+                <ul className={styles.menuItems}>
+                    <li>
+                        <a href="#home">Home</a>
+                    </li>
+                    <li>
+                        <a href="#about">About</a>
+                    </li>
+                    <li>
+                        <a href="#food">Category</a>
+                    </li>
+                    <li>
+                        <a href="#food-menu">Menu</a>
+                    </li>
+                    <li>
+                        <a href="#testimonials">Testimonial</a>
+                    </li>
+                    <li>
+                        <a href="#contact">Contact</a>
+                    </li>
+                </ul>
+                <div className={styles.logo}>
                     <Link href="/">
                         <Image
                             src="/next.svg"
@@ -18,7 +45,8 @@ export function Navbar() {
                             priority
                         />
                     </Link>
-                    <ul className="">
+                </div>
+                {/* <ul className="">
                         <li>
                             <Link href="/">
                                 <p>About Us</p>
@@ -34,9 +62,9 @@ export function Navbar() {
                                 <p>Contacts</p>
                             </Link>
                         </li>
-                    </ul>
-                </div>
+                    </ul> */}
+                {/* <h1 className="logo">RS</h1> */}
             </div>
-        </div>
+        </nav>
     )
 }
