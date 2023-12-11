f868ef81a0fbe52827b0737e3267b9603750f8b7
Nestor <zozomezo@abv.bg>
2023-12-11 20:22:16 +0200

    Testing scss

--- src/app/ui/button.js
+++ src/app/ui/button.js
@@ -1,4 +1,5 @@
-import styles from './Button.module.css'
+// import styles from './Button.module.css'
+import styles from './Button.module.scss'
 import Link from 'next/link'
 
 export function Button({ children, link, variant }) {
