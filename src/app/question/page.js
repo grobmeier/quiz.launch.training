6ffc7ca417124f7e9165b301ed0efaf2be6c863e
Nestor <zozomezo@abv.bg>
2023-12-12 19:37:36 +0200

    Question page

--- src/app/question/page.js
+++ src/app/question/page.js
@@ -8,6 +8,7 @@ export default function Page() {
             <Topbar />
             <h3>What language do you see in the picture?</h3>
             <SingleOption />
+            <SingleOption />
             {/* <div className={styles.grid}>
                 <SmallCard />
                 <SmallCard />
