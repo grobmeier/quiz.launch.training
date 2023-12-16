1131acf130c068ef77ef91563611c1f08b25624f
Nestor <zozomezo@abv.bg>
2023-12-16 22:02:08 +0200

    Initial dynamic questions

--- src/app/exams-data/data.js
+++ src/app/exams-data/data.js
@@ -1,28 +1,57 @@
-export const question1 = {
-    id: '5',
-    content: `  
+export const languagesExam = [
+    {
+        id: '5',
+        content: `  
     class HelloWorld {
             static public void main( String args[] ) {
                 System.out.println( "Hello World!");
             }
    }
     `,
-    answers: [
-        {
-            text: '1',
-            correct: false,
-        },
-        {
-            text: '3',
-            correct: false,
-        },
-        {
-            text: '5',
-            correct: false,
-        },
-        {
-            text: '6',
-            correct: true,
-        },
-    ],
-}
+        answers: [
+            {
+                text: '1',
+                correct: false,
+            },
+            {
+                text: '3',
+                correct: false,
+            },
+            {
+                text: '5',
+                correct: false,
+            },
+            {
+                text: '6',
+                correct: true,
+            },
+        ],
+    },
+    {
+        id: '1',
+        content: `
+    <ul>
+        <li>Coffee</li>
+        <li>Tea</li>
+        <li>Milk</li>
+    </ul>`,
+        answers: [
+            {
+                text: 'HTML',
+                correct: true,
+            },
+            {
+                text: 'CSS',
+                correct: false,
+            },
+            {
+                text: 'JS',
+                correct: false,
+            },
+            {
+                text: '6',
+                correct: false,
+            },
+        ],
+    },
+]
--- src/app/questions/[question]/page.js
+++ src/app/questions/[question]/page.js
@@ -1,24 +1,28 @@
-import { question1 } from '../../exams-data/data.js'
+import { languagesExam } from '../../exams-data/data.js'
 import Question from '@/app/ui/question/Question.js'
 import styles from './question-detail.module.css'
 
+export const dynamicParams = false
+
 export async function generateStaticParams() {
-    return [{ id: '5' }, { id: '2' }]
+    return languagesExam.map((p) => ({ question: p.id }))
 }
 
-export function getDataById() {
-    const data = question1
+export function getDataById(id) {
+    let dataFiltered = languagesExam.filter(function (el) {
+        return el.id === id
+    })
 
-    if (!data) {
+    if (!dataFiltered) {
         throw new Error('Failed to fetch data')
     }
+    const data = dataFiltered[0]
 
     return data
 }
 
 export default function Page({ params }) {
-    const { content, id } = getDataById()
-
+    const { id } = getDataById(params.question)
     return (
         <main className={styles.main}>
             <h1>Details about the Question</h1>
