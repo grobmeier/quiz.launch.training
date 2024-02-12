99ade5e329ff189e64cec1d0e171448a1105a235
Christian Grobmeier <cg@grobmeier.de>
2024-02-12 16:56:02 +0100

    added deploy to aws

--- _tasks/deploy.sh
+++ _tasks/deploy.sh
@@ -1,3 +1,25 @@
 #!/bin/sh
 
 npm run build
+
+BUCKET=s3://quiz.launch.training
+STANDARD="--delete --region eu-west-1"
+aws s3 sync out/ ${BUCKET} --cache-control "max-age=60" ${STANDARD}
+
+aws configure set preview.cloudfront true
+
+INVALIDATION_ID=$(date +"%s")
+INVALIDATION_JSON="{
+    \"DistributionId\": \"E3L1GO5KCNCT7W\",
+    \"InvalidationBatch\": {
+        \"Paths\": {
+            \"Quantity\": 1,
+            \"Items\": [
+                \"/*\"
+            ]
+        },
+        \"CallerReference\": \"$INVALIDATION_ID\"
+    }
+}"
+
+aws cloudfront create-invalidation --cli-input-json "$INVALIDATION_JSON"
