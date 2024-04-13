d973e8d5a32e68e3a100fe859a0798a29e0c582f
Nestor <zozomezo@abv.bg>
2024-04-13 16:49:15 +0300

    Transform Navbar css to scss

--- src/app/ui/Navbar.js
+++ src/app/ui/Navbar.js
@@ -2,7 +2,7 @@
 
 import Link from 'next/link'
 import Image from 'next/image'
-import styles from './Navbar.module.css'
+import styles from './Navbar.module.scss'
 import React, { useState, useEffect } from 'react'
 import { useContext } from 'react'
 import { ProgressContext } from '@/app/lib/QuestionProvider.js'
