28692c02f46f40b76b985c1049f058d71ceb22b4
Christian Grobmeier <cg@grobmeier.de>
2024-09-18 10:04:07 +0200

    support for storing with exam name

--- src/app/lib/Storage.js
+++ src/app/lib/Storage.js
@@ -7,19 +7,30 @@ export const Storage = {
     START_TIME: 'start_time'
 }
 
-export function read(key) {
+function prefixKey(key, examName) {
+    if (examName !== undefined) {
+        key = examName + "__" + key;
+    }
+    return key;
+}
+
+export function read(key, examName) {
+    key = prefixKey(key, examName);
     return typeof window !== "undefined" && localStorage.getItem(key);
 }
 
-export function readJSON(key) {
+export function readJSON(key, examName) {
+    key = prefixKey(key, examName);
     return JSON.parse(read(key));
 }
 
-export function put(key, value) {
+export function put(key, value, examName) {
+    key = prefixKey(key, examName);
     if (typeof value === 'object') value = JSON.stringify(value);
     return typeof window !== "undefined" && localStorage.setItem(key, value);
 }
 
-export function remove(key) {
+export function remove(key, examName) {
+    key = prefixKey(key, examName);
     return typeof window !== "undefined" && localStorage.removeItem(key);
 }
